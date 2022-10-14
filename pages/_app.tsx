8b0783bdb3655b4379387b0e0dd473c8d2363954
zettlrobert <zettl.robert@gmail.com>
2022-10-14 22:36:51 +0200

    chore(cleanup): remove next presets add some basic navigation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import NavigationBar from "../components/NavigationBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <NavigationBar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,35 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
+import PlaygroundPreviewCard from "../components/PlaygroundPreviewCard";
 
+const playgrounds = [
+  {
+    title: "Animated Spotlight",
+    description: "An animated Spotlight moving on scroll",
+    path: "animated-spotlight",
+  },
+];
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
         <title>CSS Playground</title>
-        <meta name="description" content="Playground to test animation in an isolated space" />
+        <meta
+          name="description"
+          content="Playground to test animation in an isolated space"
+        />
       </Head>
       <main>
-
+        {playgrounds.map((playground) => (
+          <PlaygroundPreviewCard
+            key={playground.title}
+            playground={playground}
+          />
+        ))}
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
