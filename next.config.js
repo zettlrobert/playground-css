213278dbd2ae8ce892efc2747b4fd2b72a09cdee
zettlrobert <zettl.robert@gmail.com>
2022-10-21 00:32:23 +0200

    feat(next-config): make next.config.js a typescript module

--- next.config.js
+++ next.config.js
@@ -1,7 +1,29 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+import path from "path";
+import { fileURLToPath } from "url";
+
+// returns the fully-resolved platform-specific Node.js file path.
+const __filename = fileURLToPath(import.meta.url);
+
+// returns the directory name of the path
+const __dirname = path.dirname(__filename);
+
+// minimize js on build
+export const swcMinify = true;
+
+// Strict Mode can be a danger when developing as it makes Components render twice
+// TODO: No strict mode when developing otherwise it should be true
+export const reactStrictMode = false;
+
+export const eslint = {
+  dirs: ["pages", "components"],
 };
 
-module.exports = nextConfig;
+/* export const experimental = { */
+/*   images: { */
+/*     layoutRaw: true, */
+/*   }, */
+/* }; */
+
+export const sassOptions = {
+  includePaths: [path.join(__dirname, "styles")],
+};
